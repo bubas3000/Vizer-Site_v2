cf61985de2e7a8d86cb0e9486c89a0927a99dd18
bubas3000 <47362581+bubas3000@users.noreply.github.com>
2021-11-08 00:37:21 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -30,7 +30,28 @@
   });
   $('.hero-slider').slickAnimation();
 
-//get all vids
+
+  /* ========================================================================= */
+  /*	Header Scroll Background Change
+  /* ========================================================================= */
+
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    //console.log(scroll);
+    if (scroll > 100) {
+      //console.log('a');
+      $('.navigation').addClass('sticky-header');
+    } else {
+      //console.log('a');
+      $('.navigation').removeClass('sticky-header');
+    }
+  });
+
+  /* ========================================================================= */
+  /*	Homepage Videos
+  /* =========================================================================  */
+
+    //get all vids
 var video =  document.querySelectorAll('video')
 
 //add source to video tag
@@ -69,76 +90,6 @@ videoSize();
 
 //note IE11 polyfill needed for each, convert to for loop.
 
-  /* ========================================================================= */
-  /*	Header Scroll Background Change
-  /* ========================================================================= */
-
-  $(window).scroll(function () {
-    var scroll = $(window).scrollTop();
-    //console.log(scroll);
-    if (scroll > 100) {
-      //console.log('a');
-      $('.navigation').addClass('sticky-header');
-    } else {
-      //console.log('a');
-      $('.navigation').removeClass('sticky-header');
-    }
-  });
-
-  /* ========================================================================= */
-  /*	Portfolio Filtering Hook
-  /* =========================================================================  */
-
-    // filter
-    setTimeout(function(){
-      var containerEl = document.querySelector('.filtr-container');
-      var filterizd;
-      if (containerEl) {
-        filterizd = $('.filtr-container').filterizr({});
-      }
-    }, 500);
-
-  /* ========================================================================= */
-  /*	Testimonial Carousel
-  /* =========================================================================  */
-
-  //Init the slider
-  $('.testimonial-slider').slick({
-    infinite: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 2000
-  });
-
-
-  /* ========================================================================= */
-  /*	Clients Slider Carousel
-  /* =========================================================================  */
-
-  //Init the slider
-  $('.clients-logo-slider').slick({
-    infinite: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    slidesToShow: 5,
-    slidesToScroll: 1
-  });
-
-
-
-
-  /* ========================================================================= */
-  /*	Company Slider Carousel
-  /* =========================================================================  */
-  $('.company-gallery').slick({
-    infinite: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    slidesToShow: 5,
-    slidesToScroll: 1
-  });
 
 
   /* ========================================================================= */
